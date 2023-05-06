559ab4617b3a159b299af289fbd8d0b95e8ffac2
Avi <c7flat13@gmail.com>
2023-05-05 22:57:27 -0700

    !

--- Code/simulation/ma_cross.py
+++ Code/simulation/ma_cross.py
@@ -7,9 +7,26 @@ NONE = 0
 get_ma_col = lambda x: f"MA_{x}"
 
 
+def load_price_data(pair, granularity, ma_list):
+    df = pd.read_pickle(f"./data/{pair}_{granularity}.pkl")
+    for ma in ma_list:
+        df[get_ma_col(ma)] = df.mid_c.rolling(window=ma).mean()
+    df.dropna(inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    return df
 
 
-def run_ma_sim(curr_list=["EUR", "USD", "JPY", "GBP", "AUD"],
+def analyse_pair(instrument, granularity, ma_long, ma_short):
+
+    ma_list = set(ma_long + ma_short)
+    pair = instrument.name
+
+    price_data = load_price_data(pair, granularity, ma_list)
+    print(pair)
+    print(price_data.head(3))
+
+
+def run_ma_sim(curr_list=["EUR", "USD", "GBP", "JPY", "AUD"],
                 granularity=["H1"],
                 ma_long=[20,40,80],
                 ma_short=[10,20]):
@@ -19,7 +36,7 @@ def run_ma_sim(curr_list=["EUR", "USD", "JPY", "GBP", "AUD"],
             for p2 in curr_list:
                 pair = f"{p1}_{p2}"
                 if pair in ic.instruments_dict.keys():
-                    print(pair)
+                    analyse_pair(ic.instruments_dict[pair], g, ma_long, ma_short)
 
 
 
