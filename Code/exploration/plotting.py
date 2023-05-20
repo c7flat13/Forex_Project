72180c8723668c96f74197f242f6d5eb9893a504
Avi <c7flat13@gmail.com>
2023-05-19 18:45:36 -0700

    !

--- Code/exploration/plotting.py
+++ Code/exploration/plotting.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 class CandlePlot:
 
@@ -9,12 +10,12 @@ class CandlePlot:
         self.create_candle_fig()
 
     def add_timestr(self):
-        self.df_plot['sTime'] = [dt.datetime.strftime(x, "s%y-%m-%d %H:%M")
-                        for x in self.df_plot.time]     
+        self.df_plot['sTime'] = [dt.datetime.strftime(x, "s%y-%m-%d %H:%M") 
+                        for x in self.df_plot.time]
 
     def create_candle_fig(self):
         self.add_timestr()
-        self.fig = go.Figure()
+        self.fig = make_subplots(specs=[[{"secondary_y": True}]])
         if self.candles == True:
             self.fig.add_trace(go.Candlestick(
                 x=self.df_plot.sTime,
@@ -24,33 +25,31 @@ class CandlePlot:
                 close=self.df_plot.mid_c,
                 line=dict(width=1), opacity=1,
                 increasing_fillcolor='#24A06B',
-                decreasing_fillcolor='#CC2E3C',
-                increasing_line_color='#2EC886',
+                decreasing_fillcolor="#CC2E3C",
+                increasing_line_color='#2EC886',  
                 decreasing_line_color='#FF3A4C'
             ))
 
     def update_layout(self, width, height, nticks):
         self.fig.update_yaxes(
             gridcolor="#1f292f"
-        )
+            )
         self.fig.update_xaxes(
             gridcolor="#1f292f",
             rangeslider=dict(visible=False),
             nticks=nticks
         )
 
-
-
         self.fig.update_layout(
             width=width,
             height=height,
-            margin=dict(l=10,r=10,t=10,b=10),
+            margin=dict(l=10,r=10,b=10,t=10),
             paper_bgcolor="#2c303c",
             plot_bgcolor="#2c303c",
-            font=dict(size=12, color="#e1e1e1")
+            font=dict(size=8, color="#e1e1e1")
         )
 
-    def add_traces(self, line_traces):
+    def add_traces(self, line_traces, is_sec=False):
         for t in line_traces:
             self.fig.add_trace(go.Scatter(
                 x=self.df_plot.sTime,
@@ -58,9 +57,10 @@ class CandlePlot:
                 line=dict(width=2),
                 line_shape="spline",
                 name=t
-            ))
+            ), secondary_y=is_sec)
 
-    def show_plot(self, width=1450, height=700, nticks=5, line_traces=[]):
+    def show_plot(self, width=900, height=400, nticks=5, line_traces=[], sec_traces=[]):
         self.add_traces(line_traces)
+        self.add_traces(sec_traces, is_sec=True)
         self.update_layout(width, height, nticks)
         self.fig.show()
